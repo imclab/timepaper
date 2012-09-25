2c1af4f24736d605c8fdef8e899a892b3130cc2d
George Michael Brower <georgebrower@google.com>
2012-09-25 14:22:07 -0700

    angular

---
+++ Entry.js
@@ -0,0 +1,39 @@
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+window.onload = function() {
+  setTimeout(function() {
+    var rect = document.querySelector('entry.today-true').getBoundingClientRect();
+    window.scrollTo(0, rect.top);
+  }, 10);
+}
+
+function EntryCtrl($scope) {
+  
+  $scope.entries = [];
+  $scope.today = new Date();
+
+
+  _.each(_.range(-15, 15), function(i) {
+    var d = new Date();
+    d.setDate($scope.today.getDate()+i);
+    $scope.entries.push(new Entry($scope, i == 0 ? $scope.today : d));
+  })
+
+};
+
+function Entry($scope, date) {
+  this.dateObject = date;
+  this.day = days[date.getDay()];
+  this.date = date.getDate();
+  this.month = months[date.getMonth()];
+  this.year = date.getFullYear();
+  this.ordinal = ordinal(this.date);
+  this.text = '';
+}
+
+function ordinal(n) {
+   var s=["th","st","nd","rd"],
+       v=n%100;
+   return (s[(v-20)%10]||s[v]||s[0]);
+}
