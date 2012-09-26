7304e6c7cd3fb8b3c943e12dbb617c9eaf8520e7
George Michael Brower <georgebrower@google.com>
2012-09-25 19:38:16 -0700

    goodman

--- Entry.js
+++ Entry.js
@@ -1,33 +1,29 @@
-var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-
-function EntryCtrl($scope) {
+angular.module('entry', ['mongolab']).
+  config(function($routeProvider) {
+    $routeProvider.
+      when('/', { controller: EntryCtrl, templateUrl: 'entry.html' }).
+      otherwise({ redirectTo: '/' });
+  });
+ 
+function EntryCtrl($scope, Entry) {
   
-  $scope.entries = [];
+  $scope.entries = Entry.query();
   $scope.today = new Date();
 
+  if ($scope.entries.length == 0) {
 
-  _.each(_.range(0, 50), function(i) {
-    var d = new Date();
-    d.setDate($scope.today.getDate()+i);
-    $scope.entries.push(new Entry($scope, i == 0 ? $scope.today : d));
-  })
+    _.each(_.range(-10, 10), function(i) {
+      var d = new Date();
+      d.setDate($scope.today.getDate()+i);
+      var e = new Entry(d);
+      $scope.entries.push(e);
+    });
+
+  }
 
 };
 
-function Entry($scope, date) {
-  this.dateObject = date;
-  this.day = days[date.getDay()];
-  this.date = date.getDate();
-  this.month = months[date.getMonth()];
-  this.year = date.getFullYear();
-  this.ordinal = ordinal(this.date);
-  this.text = '';
-}
-
 function ordinal(n) {
-   var s=["th","st","nd","rd"],
-       v=n%100;
-   return (s[(v-20)%10]||s[v]||s[0]);
+   var s = ['th','st','nd','rd'], v = n % 100;
+   return s[(v-20)%10]||s[v]||s[0];
 }
--- mongolab.js
+++ mongolab.js
@@ -1,20 +1,40 @@
-angular.module('mongolab', ['ngResource']).
-    factory('Project', function($resource) {
-      var Project = $resource('https://api.mongolab.com/api/1/databases' +
-          '/angularjs/collections/projects/:id',
-          { apiKey: '506225c4e4b0db84a758c9e3' }, {
-            update: { method: 'PUT' }
-          }
-      );
- 
-      Project.prototype.update = function(cb) {
-        return Project.update({id: this._id.$oid},
-            angular.extend({}, this, {_id:undefined}), cb);
-      };
- 
-      Project.prototype.destroy = function(cb) {
-        return Project.remove({id: this._id.$oid}, cb);
-      };
- 
-      return Project;
-    });
+angular.module('mongolab', ['ngResource']).factory('Entry', function($resource) {
+
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  var _Entry = $resource('https://api.mongolab.com/api/1/databases' +
+      '/angularjs/collections/entries/:id',
+      { apiKey: '506225c4e4b0db84a758c9e3' }, {
+        update: { method: 'PUT' },
+        save:   {method:'POST'}
+      }
+  );
+
+  var Entry = function(date) {
+    this.dateObject = date;
+    this.day = days[date.getDay()];
+    this.date = date.getDate();
+    this.month = months[date.getMonth()];
+    this.year = date.getFullYear();
+    this.ordinal = ordinal(this.date);
+    this.text = '';
+  };
+
+  // teehee
+  angular.extend(Entry, _Entry);
+
+  Entry.prototype.update = function(cb) {
+    return Entry.update({id: this._id.$oid},
+        angular.extend({}, this, {_id:undefined}), cb);
+  };
+
+  Entry.prototype.destroy = function(cb) {
+    return Entry.remove({id: this._id.$oid}, cb);
+  };
+
+  window.e = Entry;
+
+  return Entry;
+
+});
