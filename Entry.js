3faef2102707109aa31cda1e77d891316315fd60
George Michael Brower <georgebrower@google.com>
2012-09-26 11:40:10 -0700

    working

--- Entry.js
+++ Entry.js
@@ -1,19 +1,48 @@
 angular.module('entry', ['mongolab']).config(function($routeProvider) {
   $routeProvider.
-    when('/', { controller: EntryCtrl, templateUrl: 'entry.html' }).
-    otherwise({ redirectTo: '/' });
+  when('/', { controller: EntryCtrl, templateUrl: 'entry.html' }).
+  otherwise({ redirectTo: '/' });
 });
 
-angular.module('entry').directive('keyUpdate', function() {
+angular.module('entry').directive('expandingArea', function() {
+
   return function($scope, element, attrs) {
-    var el = element[0];
-    el.addEventListener('keyup', function() {
-      $scope.entry.text = el.innerHTML;
-      $scope.entry.update(function() {
-        console.log('done');
+
+    angular.forEach(element, function(e) {
+
+      e.classList.add('expandingArea');
+
+      var area = e.querySelector('textarea');
+      var span = e.querySelector('span');
+      var change = function() {
+        span.textContent = area.value;
+      };
+
+      area.addEventListener('input', change, false);
+
+      _.defer(change);
+
+      e.classList.add('active');
+
+      area.addEventListener('focus', function() {
+        e.parentElement.classList.add('focus');
+      });
+      area.addEventListener('blur', function() {
+        e.parentElement.classList.remove('focus');
+      });
+
+      _.defer(function() {
+
+        if (e.parentElement.classList.contains('today')) {
+          var rect = e.parentElement.getBoundingClientRect();
+          window.scrollTo(0, rect.top - window.innerHeight/2 + rect.height);
+        }
       })
-    }, false);
+
+
+    });
   }
+
 });
 
 function EntryCtrl($scope, Entry) {
@@ -22,36 +51,48 @@ function EntryCtrl($scope, Entry) {
   var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   
   var millis = 86400000;
-  $scope.today = new Date(Math.floor(Date.now()/millis)*millis);
+  var today = new Date(Math.floor(Date.now()/millis)*millis);
+  $scope.today = today.getTime();
   $scope.entries = Entry.query({}, function() {
-
-    console.log($scope.entries.length);
 
     if ($scope.entries.length == 0) {
 
-      _.each(_.range(-10, 10), function(i) {
+      angular.forEach(_.range(-20, 20), function(i) {
+        
         var date = new Date();
-        date.setDate($scope.today.getDate()+i);
+        date.setDate(today.getDate()+i);
+
+        console.log(i);;
+
+
         var e = new Entry({
-          index: Math.floor(date.getTime()/millis),
+          
+          time: Math.floor(date.getTime()/millis)*millis,
+
           day: days[date.getDay()],
           date: date.getDate(),
           month: months[date.getMonth()],
           year: date.getFullYear(),
           ordinal: ordinal(this.date),
-          text: ' '
+          text: ' ',
+          offset: i
+
         });
+
+
+
         e.$save();
         $scope.entries.push(e);
+
       });
 
     }
-
+    
   });
 
 };
 
 function ordinal(n) {
-   var s = ['th','st','nd','rd'], v = n % 100;
-   return s[(v-20)%10]||s[v]||s[0];
+ var s = ['th','st','nd','rd'], v = n % 100;
+ return s[(v-20)%10]||s[v]||s[0];
 }
--- mongolab.js
+++ mongolab.js
@@ -1,7 +1,6 @@
 angular.module('mongolab', ['ngResource']).factory('Entry', function($resource) {
 
-
-  var Entry = $resource('https://api.mongolab.com/api/1/databases/timepaper/collections/entries5/:id',
+  var Entry = $resource('https://api.mongolab.com/api/1/databases/timepaper/collections/entries11/:id',
       { apiKey: '506225c4e4b0db84a758c9e3' }, {
         update: { method: 'PUT' },
         save: { method: 'POST' }
@@ -9,7 +8,6 @@ angular.module('mongolab', ['ngResource']).factory('Entry', function($resource) 
   );
 
   Entry.prototype.update = function(cb) {
-    console.log(this);
     return Entry.update({id: this._id.$oid},
         angular.extend({}, this, {_id:undefined}), cb);
   };
