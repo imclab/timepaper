5f36e164eb9aae3f8ab44e89f26049dc333d1a2d
George Michael Brower <georgebrower@google.com>
2012-10-10 19:58:18 -0700

    iphone

--- src/app.js
+++ src/app.js
@@ -73,7 +73,7 @@ function EntryCtrl($scope, $route, $routeParams, Entry) {
 
   $scope.solo = function(entry) {
     
-    if (Modernizr.touch) {
+    if (true || Modernizr.touch) {
       window.location = '#/'+$routeParams.tableId+'/'+entry._id.$oid;
     } else if ($scope.month) {
       $scope.drawer = true;
